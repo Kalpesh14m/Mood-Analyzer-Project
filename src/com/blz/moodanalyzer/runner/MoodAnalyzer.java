fd60779d3c53cae657b2574efd009ced1d226575
kalpesh-mali <kalpesh.mali@bridgelabz.com>
2020-10-06 16:12:30 +0530

    Added: Hello world msg to test project config

--- src/com/blz/moodanalyzer/runner/MoodAnalyzer.java
+++ src/com/blz/moodanalyzer/runner/MoodAnalyzer.java
@@ -3,16 +3,5 @@ package com.blz.moodanalyzer.runner;
 public class MoodAnalyzer {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
-		MoodAnalyzer myMood = new MoodAnalyzer();
-		boolean flag = myMood.analyseMood("sad");
-
-		System.out.println("My Mood: " + flag);
-
-	}
-
-	public boolean analyseMood(String msg) {
-		if (msg.equalsIgnoreCase("happy"))
-			return true;
-		return false;
 	}
 }
