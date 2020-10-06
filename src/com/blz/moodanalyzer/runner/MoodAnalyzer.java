210878e35701b68a506809ba9191a32910c0c6e7
kalpesh-mali <kalpesh.mali@bridgelabz.com>
2020-10-06 16:33:41 +0530

    Modified: user Mood is Sad

--- src/com/blz/moodanalyzer/runner/MoodAnalyzer.java
+++ src/com/blz/moodanalyzer/runner/MoodAnalyzer.java
@@ -4,14 +4,14 @@ public class MoodAnalyzer {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 		MoodAnalyzer myMood = new MoodAnalyzer();
-		boolean flag = myMood.analyseMood("sad");
+		boolean flag = myMood.analyseMood("Sad");
 
 		System.out.println("My Mood: " + flag);
 
 	}
 
 	public boolean analyseMood(String msg) {
-		if (msg.equalsIgnoreCase("happy"))
+		if (msg.contains("Sad"))
 			return true;
 		return false;
 	}
